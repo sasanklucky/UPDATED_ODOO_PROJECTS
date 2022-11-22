7c322bf6ee5b4e04bded8066b970ebeef2d6b753
surya <surya.tripathy@csm.co.in>
2022-11-22 22:03:30 +0530

    report changes

--- ars_vehicle_sales/models/ars_change.py
+++ ars_vehicle_sales/models/ars_change.py
@@ -5,6 +5,11 @@ from odoo.addons import decimal_precision as dp
 from odoo.exceptions import UserError
 from odoo.addons import decimal_precision as dp
 
+class arsCompany(models.Model):
+    _inherit = 'res.company'
+
+    dealer_code = fields.Char(string="Dealer Code")
+    
 class ars_sale_crm_lead(models.Model):
     _inherit = 'crm.lead'
     product_id = fields.Many2one('product.product', string='Product', domain=[('sale_ok', '=', True)],
@@ -16,6 +21,39 @@ class ars_sale_crm_sale(models.Model):
     def _compute_amount_total_words(self):
         for sale in self:
             sale.amount_total_words = sale.currency_id.amount_to_text(sale.amount_total)
+
+    @api.multi
+    def _get_proforma_invoice_types(self):
+        self.ensure_one()
+        proforma_type = ''
+        if len(self.order_line) == 1:
+            for line in self.order_line:
+                # print("catalog",line.product_id.catalog_type.name)
+                if line.product_id.catalog_type.name == 'Vehicle':
+                    proforma_type = 'vehicle'
+        # print("proforma",proforma_type)
+        return proforma_type
+
+            
+    @api.multi
+    def _get_tax_amount_by_group_wise(self):
+        self.ensure_one()
+        res = {}
+        for line in self.order_line:
+            price_reduce = line.price_unit * (1.0 - line.discount / 100.0)
+            taxes = line.tax_id.compute_all(price_reduce, quantity=line.product_uom_qty, product=line.product_id, partner=self.partner_shipping_id)['taxes']
+            for tax in line.tax_id:
+                group = tax.tax_group_id
+                res.setdefault(group, {'amount': 0.0, 'base': 0.0})
+                for t in taxes:
+                    if t['id'] == tax.id or t['id'] in tax.children_tax_ids.ids:
+                        res[group]['name'] = tax.name
+                        res[group]['amount'] += t['amount']
+                        res[group]['base'] += t['base']
+        res = sorted(res.items(), key=lambda l: l[0].sequence)
+        res = [(l[1]['name'], l[1]['amount'], l[1]['base'], len(res)) for l in res]
+        # print(res)
+        return res
 
     @api.depends('state', 'user_id')
     def _enable_approve_button(self):
