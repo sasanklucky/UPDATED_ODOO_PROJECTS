b53026859e5638ce602d2ca74ec4971c333fca72
Amal Autochip <amal.mohan@autochip.in>
2023-04-02 15:18:37 +0530

    [M] Error Fix

--- ac_product_catalog/__manifest__.py
+++ ac_product_catalog/__manifest__.py
@@ -17,7 +17,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/purchase_order.xml',
-        'views/stock_picking.xml',
+        # 'views/stock_picking.xml',
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
