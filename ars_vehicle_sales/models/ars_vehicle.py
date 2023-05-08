64fb3d52bd9c325954cc1a7e2bb67556395b523b
Amal Autochip <amal.mohan@autochip.in>
2023-05-08 09:56:14 +0530

    [M]DMS Updation 5/5/23

--- ars_vehicle_sales/models/ars_vehicle.py
+++ ars_vehicle_sales/models/ars_vehicle.py
@@ -54,7 +54,8 @@ class FleetVehicle(models.Model):
     service_ids = fields.One2many('service.history', 'vehicle_id')
     lot_id = fields.Many2one('stock.production.lot', 'Stock Production Lot ')
     license_plate = fields.Char(required=False, help='License plate number of the vehicle (i = plate number for a car)')
-
+    driver_id = fields.Many2one('res.partner', 'Customer', track_visibility="onchange", help='Customer of the vehicle',
+                                copy=False)
     # engine_type_code = fields.Char(string='Engine Type Code', related="product_id.product_tmpl_id.engine_type_code")
     # no_of_cylinder = fields.Char(string='No of Cylinder', related="product_id.product_tmpl_id.no_of_cylinder")
     # cylinder_capacity = fields.Char(string='Cylinder Capacity', related="product_id.product_tmpl_id.cylinder_capacity")
