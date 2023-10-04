1b552196926bd2fa8131d31259710f6a73c6b2ba
Amal Autochip <amal.mohan@autochip.in>
2023-10-04 17:32:08 +0530

    Accounting report Change[M]

--- ars_after_sales/models/ars_account_invoice.py
+++ ars_after_sales/models/ars_account_invoice.py
@@ -110,7 +110,8 @@ class ARS_account_invoice(models.Model):
     reg_no = fields.Many2one('fleet.vehicle', string="Regn No")
     vin = fields.Char(string="VIN")
     model = fields.Many2one('product.product')
-    kilometer = fields.Integer(string="Kilometer")
+    kilometer = fields.Float(string="Kilometer")
+    kilometer_out = fields.Float(string="Kilometer Out")
     delivery_service_advisor = fields.Many2one('res.users')
     doc_type = fields.Selection([
         ('appointment', 'Appointment'),
