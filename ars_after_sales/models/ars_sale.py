760aeb9dcd8b1ad81ffd3aae44ff6d6b0b9e07d7
Amal Autochip <amal.mohan@autochip.in>
2023-11-24 19:57:48 +0530

    Purchase Order [M]

--- ars_after_sales/models/ars_sale.py
+++ ars_after_sales/models/ars_sale.py
@@ -660,10 +660,16 @@ class ARSPurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
     _description = 'Purchase Order Line'
 
+    def onchange_order_line(self):
+        length = 1
+        for line in self:
+            line.sl_no = length
+            length = length + 1
+
     product_template_id = fields.Many2one('product.template', string='Product')
     product_catalog_id = fields.Many2one('product.catalog', string='Catalog Type')
     product_id_domain = fields.Char(compute="_compute_product_id_domain", readonly=True, store=False)
-
+    sl_no = fields.Integer(compute="onchange_order_line")
     @api.multi
     @api.depends('product_catalog_id')
     def _compute_product_id_domain(self):
