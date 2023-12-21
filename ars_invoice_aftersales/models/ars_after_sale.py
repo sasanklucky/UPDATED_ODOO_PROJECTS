6eef1128a3a1495bd0d9bb1902f29892e276d502
Amal Autochip <amal.mohan@autochip.in>
2023-12-21 14:53:11 +0530

    New Update on Thu3

--- ars_invoice_aftersales/models/ars_after_sale.py
+++ ars_invoice_aftersales/models/ars_after_sale.py
@@ -29,7 +29,7 @@ class ARS_After_sale_order(models.Model):
                                                                ('service_type','=',self.service_type.id)])
             if vehicle:
                 if self.regn_no.customer_ids:
-                    customer_ids_fleet = self.fleet_vin_no.customer_ids[-1]  # selecting the last record of one2many field customer_ids
+                    customer_ids_fleet = self.regn_no.customer_ids[-1]  # selecting the last record of one2many field customer_ids
                     if customer_ids_fleet.date_of_ownership:
                         date_ownership = fields.Date.from_string(customer_ids_fleet.date_of_ownership)  # assigning the value of date of ownership to a varaible
                         current_date = date.today()
