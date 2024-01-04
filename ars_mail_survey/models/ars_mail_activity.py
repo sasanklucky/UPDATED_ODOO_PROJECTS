af308c6b67ef3d3fed25b3c89b9fdd1a4e20d94c
Suresh Chellam <39961028+chellaminfotech@users.noreply.github.com>
2024-01-04 17:23:45 +0530

    Update ars_mail_activity.py

--- ars_mail_survey/models/ars_mail_activity.py
+++ ars_mail_survey/models/ars_mail_activity.py
@@ -11,7 +11,7 @@ class ARS_MailActivity(models.Model):
     _name = 'mail.activity'
     _inherit = ['mail.activity', 'mail.thread']
 
-    company_id = fields.Many2one('res.company', related="invoice_id.company_id", store=False)
+    company_id = fields.Many2one('res.company', related="invoice_id.company_id", store=True)
 
     @api.multi
     @api.depends('response_id', 'survey_percentage')
