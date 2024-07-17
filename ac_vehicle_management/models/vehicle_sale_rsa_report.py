1922d5f694ddc38490d1331cccac30549afdfc59
Amal Autochip <amal.mohan@autochip.in>
2024-07-17 18:58:25 +0530

    RSA Report Update[M]

--- ac_vehicle_management/models/vehicle_sale_rsa_report.py
+++ ac_vehicle_management/models/vehicle_sale_rsa_report.py
@@ -44,58 +44,78 @@ class VehicleSalePsfReport(models.Model):
     def init(self):
         tools.drop_view_if_exists(self.env.cr, self._table)
         self.env.cr.execute(f""" CREATE or REPLACE VIEW {self._table} as (
-            select row_number() over(order by inv.id desc) as id,
-                invline.product_template_id,
-                brand_name.name AS brand_name,
-                inv.number AS policy_number,
-                inv.date_invoice AS start_date,
-                (inv.date_invoice + INTERVAL '6 years')::date AS end_date,
-                inv.partner_id AS customer_name,
-                invline.product_template_id AS Model_Type,
-                dealer.dealer_code AS selling_dealer_code,
-                dealer.name AS dealer_name,
-                rp.street AS dealer_address,
-                rp.street2 AS dealer_address1,
-                rp.city AS dealer_city,
-                rp.state_id AS dealer_state,
-                rp.zip AS dealer_pin,
-                rp.mobile AS dealer_mobile,
-                rp.email AS dealer_email,
-                lot.name as vin_number,
-                fv.initial_reg_no as reg_no,
-                pav.name AS color_id
-            from 
-                account_invoice inv
-            left join 
-                account_invoice_line invline ON invline.invoice_id = inv.id
-            left join 
-                product_template pt ON pt.id = invline.product_template_id
-            left join 
-                stock_production_lot lot on invline.vin_no = lot.id
-            left join
-                fleet_vehicle fv on fv.lot_id = invline.vin_no
-            left join 
-                fleet_vehicle_model_brand brand_name ON brand_name.id = pt.brand_id
-            left join 
-                res_company AS dealer ON dealer.id = invline.company_id
-            left join 
-                res_partner AS rp ON rp.id = dealer.partner_id
-			left join 
-				product_product as pp on pp.id = invline.product_id
-			left join 
-			    product_attribute_value_product_product_rel as pvc on pvc.product_product_id = pp.id
-			left join 
-			    product_attribute_value as pav on pav.id = pvc.product_attribute_value_id
-			left join 
-			    product_attribute as pa on pa.id = pav.attribute_id
-            where 
-                inv.id = invline.invoice_id
-                and inv.type='out_invoice'
-                and inv.ars_invoice_type = 'vehicle' 
-                and invline.vin_no IS NOT NULL 
-                and inv.state not in ('draft', 'cancel')
-				and LOWER(pa.name) NOT LIKE 'int%')
-            """)
+                select row_number() over(order by inv.id desc) as id,
+                    invline.product_template_id,
+    --           pt.name,
+                    brand_name.name AS brand_name,
+                    inv.number AS policy_number,
+                    inv.date_invoice AS start_date,
+                    (inv.date_invoice + INTERVAL '6 years')::date AS end_date,
+                    inv.partner_id AS customer_name,
+                    invline.product_template_id AS Model_Type,
+                    dealer.dealer_code AS selling_dealer_code,
+                    dealer.name AS dealer_name,
+                    rp.street AS dealer_address,
+                    rp.street2 AS dealer_address1,
+                    rp.city AS dealer_city,
+                    rp.state_id AS dealer_state,
+                    rp.zip AS dealer_pin,
+                    rp.mobile AS dealer_mobile,
+                    rp.email AS dealer_email,
+                    lot.name as vin_number,
+                    fv.initial_reg_no as reg_no,
+    --                 pav.name AS color_id
+                 prod_attrs.Exterior_Color
+                from 
+                    account_invoice inv
+                left join 
+                    account_invoice_line invline ON invline.invoice_id = inv.id
+                left join 
+                    product_template pt ON pt.id = invline.product_template_id
+                left join 
+                    stock_production_lot lot on invline.vin_no = lot.id
+                left join
+                    fleet_vehicle fv on fv.lot_id = invline.vin_no
+                left join 
+                    fleet_vehicle_model_brand brand_name ON brand_name.id = pt.brand_id
+                left join 
+                    res_company AS dealer ON dealer.id = invline.company_id
+                left join 
+                    res_partner AS rp ON rp.id = dealer.partner_id
+              left join 
+                 product_product as pp on pp.id = invline.product_id
+              left join (SELECT * FROM crosstab(
+                  'select inv.id as inv_id ,
+                    CASE WHEN attr.name LIKE ''_xt%'' THEN ''Exterior Color'' ELSE ''Interior Color'' END AS attr,
+                       attr_val.name AS attrs_val
+                       from account_invoice inv
+                       left join account_invoice_line lines on inv.id = lines.invoice_id
+                       left join product_product pp on lines.product_id = pp.id    
+                       left join product_template pt on pp.product_tmpl_id = pt.id
+                       left join product_catalog cat on pt.catalog_type= cat.id
+                       left join product_attribute_value_product_product_rel attr_rel on pp.id = attr_rel.product_product_id
+                       left join product_attribute_value attr_val on attr_rel.product_attribute_value_id = attr_val.id
+                       left join product_attribute attr on attr_val.attribute_id = attr.id
+                          ORDER BY inv.id',
+                     'SELECT DISTINCT CASE WHEN attr.name LIKE ''_xt%'' THEN ''Exterior Color'' ELSE ''Interior Color'' END AS attr 
+                     FROM product_attribute_value attr_val
+                     LEFT JOIN product_attribute attr ON attr_val.attribute_id = attr.id'
+                    ) AS newtable (inv_id INT, Exterior_Color VARCHAR, Interior_Color VARCHAR)) as prod_attrs on prod_attrs.inv_id = inv.id
+    --        left join 
+    --            product_attribute_value_product_product_rel as pvc on pvc.product_product_id = pp.id
+    --        left join 
+    --            product_attribute_value as pav on pav.id = pvc.product_attribute_value_id
+    --        left join 
+    --           product_attribute as pa on pa.id = pav.attribute_id
+                where 
+                    inv.id = invline.invoice_id
+                    and inv.type='out_invoice'
+                    and inv.ars_invoice_type = 'vehicle' 
+                    and invline.vin_no IS NOT NULL 
+                    and inv.state not in ('draft', 'cancel')
+    --           and LOWER(pa.name) NOT LIKE 'int%'
+                 and pt.rsa = 'yes'
+                """)
 
     def export_xls_rsa(self, param=None):
         output = io.BytesIO()
