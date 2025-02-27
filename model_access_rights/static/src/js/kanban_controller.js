8eb12d74c9e54fb532619079236b291fa4029f3a
krishna <krishna.chaitanya@autochip.in>
2025-02-27 18:09:34 +0530

    kanban view hide delete,hide.

--- model_access_rights/static/src/js/kanban_controller.js
+++ model_access_rights/static/src/js/kanban_controller.js
@@ -1,183 +1,60 @@
-////odoo.define('model_access_rights.kanban_controller', function (require) {
-////    "use strict";
-////
-////    var KanbanRecord = require('web.KanbanRecord');
-////    var rpc = require('web.rpc');
-////
-////    KanbanRecord.include({
-////        willStart: function () {
-////            var self = this;
-////            self.removedActions = [];
-////
-////            return this._super.apply(this, arguments).then(function () {
-////                return rpc.query({
-////                    model: 'access.right',
-////                    method: 'hide_buttons',
-////                    args: [],
-////                    kwargs: {
-////                        model: self.modelName,
-////                        user_id: self.getSession().uid,
-////                    },
-////                }).then(function (records) {
-////                    console.log("Fetched records:", records);
-////
-////                    if (records.is_delete) self.removedActions.push("Delete");
-////                    if (records.is_archive) self.removedActions.push("Archive");
-////                    if (records.is_edit) self.removedActions.push("Edit");
-////
-////                    console.log("Removed Actions:", self.removedActions);
-////                }).fail(function (error) {
-////                    console.error("RPC Error:", error);
-////                });
-////            });
-////        },
-////
-////        _render: function () {
-////            this._super.apply(this, arguments);
-////
-////            var self = this;
-////            setTimeout(function () {
-////                var dropdown = self.$('.o_dropdown_kanban .dropdown-menu');
-////                if (!dropdown.length) return;
-////
-////                dropdown.find('a').each(function () {
-////                    var actionLabel = $(this).text().trim();
-////                    if (self.removedActions.includes(actionLabel)) {
-////                        $(this).remove();
-////                    }
-////                });
-////
-////            }, 100);
-////        }
-////
-////    });
-////
-////});
-////
-//odoo.define('model_access_rights.kanban_controller', function (require) {
-//    "use strict";
-//
-//    var KanbanRecord = require('web.KanbanRecord');
-//    var rpc = require('web.rpc');
-//
-//    KanbanRecord.include({
-//        willStart: function () {
-//            var self = this;
-//            self.removedActions = [];
-//
-//            return this._super.apply(this, arguments).then(function () {
-//                return rpc.query({
-//                    model: 'access.right',
-//                    method: 'hide_buttons',
-//                    args: [],
-//                    kwargs: {
-//                        model: self.modelName,
-//                        user_id: self.getSession().uid,
-//                    },
-//                }).then(function (records) {
-//                    console.log("Fetched records:", records);
-//
-//                    if (records.is_delete) self.removedActions.push("Delete");
-//                    if (records.is_archive) self.removedActions.push("Archive");
-//                    if (records.is_edit) self.removedActions.push("Edit");
-//
-//                    console.log("Removed Actions:", self.removedActions);
-//                }).fail(function (error) {
-//                    console.error("RPC Error:", error);
-//                });
-//            });
-//        },
-//
-//        start: function () {
-//            var self = this;
-//            return this._super.apply(this, arguments).then(function () {
-//                self._removeActionsFromDropdown();
-//            });
-//        },
-//
-//        _removeActionsFromDropdown: function () {
-//            var self = this;
-//            setTimeout(function () {
-//                var dropdown = self.$('.o_dropdown_kanban .dropdown-menu');
-//                if (!dropdown.length) return;
-//
-//                dropdown.find('a').each(function () {
-//                    var actionLabel = $(this).text().trim();
-//                    if (self.removedActions.includes(actionLabel)) {
-//                        $(this).remove();
-//                    }
-//                });
-//
-//            }, 100);
-//        }
-//    });
-//});
 odoo.define('model_access_rights.kanban_controller', function (require) {
     "use strict";
 
-    var KanbanRecord = require('web.KanbanRecord');
+    var KanbanController = require('web.KanbanController');
     var rpc = require('web.rpc');
 
-    KanbanRecord.include({
-        willStart: function () {
-            var self = this;
-            self.removedActions = [];
-
-            // **Skip execution if the model is 'ir.module.module' (Apps menu)**
-            if (self.modelName === 'ir.module.module') {
-                console.log("Skipping Apps Kanban View (willStart)");
-                return this._super.apply(this, arguments);
-            }
-
-            return this._super.apply(this, arguments).then(function () {
-                return rpc.query({
-                    model: 'access.right',
-                    method: 'hide_buttons',
-                    args: [],
-                    kwargs: {
-                        model: self.modelName,
-                        user_id: self.getSession().uid,
-                    },
-                }).then(function (records) {
-                    console.log("Fetched records:", records);
-
-                    if (records.is_delete) self.removedActions.push("Delete");
-                    if (records.is_archive) self.removedActions.push("Archive");
-                    if (records.is_edit) self.removedActions.push("Edit");
-
-                    console.log("Removed Actions:", self.removedActions);
-                }).fail(function (error) {
-                    console.error("RPC Error:", error);
-                });
-            });
-        },
-
-        _render: function () {
-            var self = this;
+    KanbanController.include({
+        renderButtons: function () {
             this._super.apply(this, arguments);
+            var self = this;
 
-            // **Skip execution if the model is 'ir.module.module' (Apps menu)**
-            if (self.modelName === 'ir.module.module') {
-                console.log("Skipping Apps Kanban View (_render)");
-                return;
-            }
+            // Fetch user permissions only once
+            rpc.query({
+                model: 'access.right',
+                method: 'hide_buttons',
+                args: [],
+                kwargs: {
+                    model: self.modelName,
+                    user_id: self.getSession().uid,
+                },
+            }).then(function (records) {
+                var removedActions = [];
+                if (records.is_delete) removedActions.push("Delete");
+                if (records.is_archive) removedActions.push("Archive");
+                if (records.is_edit) removedActions.push("Edit");
 
-            setTimeout(function () {
-                var dropdown = self.$('.o_dropdown_kanban .dropdown-menu');
-                if (!dropdown.length) return;
+//                console.log("Removed Actions:", removedActions);
 
-                dropdown.find('a').each(function () {
-                    var actionLabel = $(this).text().trim();
-                    if (self.removedActions.includes(actionLabel)) {
-                        $(this).remove(); // Hide instead of remove
-                    }
-                });
+                // Apply the changes to all Kanban dropdowns after rendering
+                setTimeout(function () {
+                    var dropdowns = self.$el.find('.o_dropdown_kanban .dropdown-menu');
 
-            }, 500);
+                    if (!dropdowns.length) return;
+
+                    dropdowns.each(function () {
+                        var dropdown = $(this);
+
+                        dropdown.find('a').each(function () {
+                            var actionLabel = $(this).text().trim();
+                            if (removedActions.includes(actionLabel)) {
+                                $(this).remove(); // Remove the restricted actions
+                            }
+                        });
+                    });
+
+                }, 500); // Delay for proper dropdown rendering
+
+            }).fail(function (error) {
+                console.error("RPC Error:", error);
+            });
+
         }
     });
 
 });
+
+
 
 
 
