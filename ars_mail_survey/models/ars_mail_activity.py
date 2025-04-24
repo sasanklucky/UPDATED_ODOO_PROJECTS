b3cc7bb0fdbd3896296e84543615e78faabc6ec0
Johney <john.britto@autochip.in>
2025-04-24 15:31:46 +0530

    DMS - Response Create Date (field and function) in Mail Activity model

--- ars_mail_survey/models/ars_mail_activity.py
+++ ars_mail_survey/models/ars_mail_activity.py
@@ -20,6 +20,20 @@ class ARS_MailActivity(models.Model):
         [('satisfied', 'Satisfied'), ('dissatisfied', 'Dissatisfied')],
         string="Survey Status", )
 
+    response_create_date = fields.Datetime(
+        string="Survey Complete Date",
+        compute="_compute_response_create_date",
+        store=True
+    )
+
+    @api.depends('response_id.create_date')
+    def _compute_response_create_date(self):
+        for rec in self:
+            if rec.response_id:
+                rec.response_create_date = rec.response_id.create_date
+            else:
+                rec.response_create_date = False
+
     @api.model
     def create(self, values):
         res = super(ARS_MailActivity, self).create(values)
