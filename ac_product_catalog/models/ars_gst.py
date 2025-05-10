66d932f0534fc7f790ff384d38e7fbd5de520430
Johney <john.britto@autochip.in>
2025-05-10 16:13:06 +0530

    Dms - GSTIN Validation (space Issue Resolved)

--- ac_product_catalog/models/ars_gst.py
+++ ac_product_catalog/models/ars_gst.py
@@ -20,8 +20,8 @@ class ARSGST(models.Model):
     def _check_gstin_format(self):
         for record in self:
             if record.vat:
-                gstin = record.vat.strip()
-                print('GSTIN:', gstin)
+                gstin = record.vat
+                # print('GSTIN:', gstin)
 
                 if len(gstin) != 15:
                     raise ValidationError("GSTIN must be exactly 15 characters long.")
@@ -30,21 +30,21 @@ class ARSGST(models.Model):
                     raise ValidationError("GSTIN must start with a valid numeric state code.")
 
                 state_code = gstin[:2]
-                print('State Code from GSTIN:', state_code)
+                # print('State Code from GSTIN:', state_code)
 
                 if record.state_id:
                     if record.state_id.code != state_code:
                         raise ValidationError(
                             f"GSTIN's state code ({state_code}) does not match the selected state code ({record.state_id.code})."
                         )
-                    print('Valid GSTIN for selected state:', record.state_id.code)
+                    # print('Valid GSTIN for selected state:', record.state_id.code)
                 else:
                     state = self.env['res.country.state'].search([('code', '=', state_code)], limit=1)
                     if not state:
                         raise ValidationError(
                             f"Invalid GSTIN: The first two digits ({state_code}) do not match any state code."
                         )
-                    print('Valid GSTIN without state_id:', state.code)
+                    # print('Valid GSTIN without state_id:', state.code)
 
                 gstin_pattern = r'^\d{2}[A-Z]{5}\d{4}[A-Z0-9]{2}Z[A-Z0-9]$'
                 if not re.match(gstin_pattern, gstin):
