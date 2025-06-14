3b8832b40525b0436cb9f94a7d689197234aa434
Johney <john.britto@autochip.in>
2025-06-14 11:08:14 +0530

    DMS - pdf report added the wizard report.

--- ars_reports/models/retail_report.py
+++ ars_reports/models/retail_report.py
@@ -82,10 +82,26 @@ class RetailReport(models.Model):
                     ((record.customer_name.country_id.name + ',') if record.customer_name.country_id else '') + \
                     ((record.customer_name.zip) if record.customer_name.zip else '')
 
-    @api.model_cr
-    def init(self):
+    @api.multi
+    def sql_query(self, companys, start_date, end_date):
         tools.drop_view_if_exists(self.env.cr, self._table)
-        self.env.cr.execute(f""" CREATE or REPLACE VIEW %s as (
+        if len(companys) == 1:
+            company_ids = f"({companys[0]})"
+        else:
+            company_ids = str(tuple(companys))
+
+        date_filter = ""
+        if start_date and end_date:
+            if isinstance(start_date, str):
+                start_date = fields.Datetime.from_string(start_date)
+            if isinstance(end_date, str):
+                end_date = fields.Datetime.from_string(end_date)
+            start_date_str = "'{}'".format(start_date.strftime('%Y-%m-%d %H:%M:%S'))
+            end_date_str = "'{}'".format(end_date.strftime('%Y-%m-%d %H:%M:%S'))
+            date_filter = f"AND ai.date_invoice::date BETWEEN {start_date_str} AND {end_date_str}"
+
+        self.env.cr.execute(f"""
+                    CREATE OR REPLACE VIEW {self._table} AS (
             select row_number() over(order by ai.id desc) as id,
             ai.company_id as dealer_id,
             rc.dealer_code as dealer_code,
@@ -130,9 +146,10 @@ class RetailReport(models.Model):
             left join product_template pt on pt.id = ail.product_template_id
             left join model_groups mg on mg.id = pt.master_id       
             where ai.type = 'out_invoice' and ct.team_type = 'sales' and ai.ars_invoice_type = 'vehicle'
-            
-        )""" % (self._table))
-
+                    AND ai.company_id IN {company_ids}
+                        {date_filter}
+            )
+        """)
 
 # from lxml import etree
 class ResPartner(models.Model):
@@ -153,3 +170,27 @@ class ResPartner(models.Model):
         self._add_tracking_to_fields()
         return super(ResPartner, self)._register_hook()
 
+
+class RetailReportPSFWizard(models.TransientModel):
+    _name = 'retail.report.wizard'
+
+    company_id = fields.Many2many('res.company', default=lambda self: self.env.user.company_ids)
+    start_date = fields.Datetime(string='Start Date')
+    end_date = fields.Datetime(string='End Date')
+
+    def retail_report_qty(self):
+        self.ensure_one()
+        comp_ids = []
+        for rec in self.company_id:
+            comp_ids.append(rec.id)
+        query = self.env['retail.report']
+        query.sudo().sql_query(companys=comp_ids,start_date=self.start_date, end_date=self.end_date)
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Retail Report',
+            'res_model': 'retail.report',
+            'view_mode': 'tree',
+            'view_type': 'form',
+            'context': self.env.context,
+            'target': 'current',
+        }
