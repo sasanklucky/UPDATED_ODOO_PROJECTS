d2b83c96ac2c2b2de8e645c8e284e18afd11143e
krishna <krishna.chaitanya@autochip.in>
2025-06-17 11:32:15 +0530

    CRM issues as mentioned in spreadsheet.

--- ac_vehicle_management/static/src/js/crm_lead_hide_edit_V.js
+++ ac_vehicle_management/static/src/js/crm_lead_hide_edit_V.js
@@ -10,8 +10,7 @@ odoo.define('ac_vehicle_management.crm_lead_hide_edit_V', function(require) {
             if (this.modelName === 'crm.lead' && this.$buttons) {
                 var activeField = this.renderer.state.data.active;
                 var teamField = this.renderer.state.data.team_id.data.display_name;
-                console.log(teamField);
-                if (activeField === false && teamField === 'Sales') {
+                if (activeField === false && teamField.toLowerCase().startsWith('sales') && teamField.toLowerCase().startsWith('sale')) {
                     this.$buttons.find('.o_form_button_edit').hide();
                 } else {
                     this.$buttons.find('.o_form_button_edit').show();  // ensure it appears again if needed
