d2cd3cb5db8b299bf9f48f7d7c5a9dd3347d6d22
pmjautochip <praveen@autochip.in>
2025-07-30 12:37:19 +0530

    changed the view name in model group product_template and required field.And removed depands module from model group

--- model_group/__manifest__.py
+++ model_group/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '1.0',
     'summary': 'Adds Master Product concept',
     "author": "Autochip",
-    'depends': ['base','product','stock','ars_after_sales','ars_vehicle_sales','ac_vehicle_management', 'ars_reports', 'fleet','account'],
+    'depends': ['base','product','stock','ars_after_sales','ars_vehicle_sales', 'fleet','account'],
     'data': [
         'security/ir.model.access.csv',
         'security/security.xml',
