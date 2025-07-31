a6fa6e80bbf810b22b946e7d2f22a112d60a7e49
Amal Autochip <amal.mohan@autochip.in>
2025-07-31 13:20:27 +0530

    Print Commanded[M]

--- ac_vehicle_history/wizard/return_validation.py
+++ ac_vehicle_history/wizard/return_validation.py
@@ -9,22 +9,22 @@ class ARSStockReturnPickings(models.TransientModel):
     def _create_returns(self):
         res = super(ARSStockReturnPickings, self)._create_returns()
         picking_record = self.env['stock.picking'].sudo().search([('id', '=', self.picking_id.id)])
-        print(picking_record,'picking_record')
-        print(picking_record.move_lines,'move_lines')
+        # print(picking_record,'picking_record')
+        # print(picking_record.move_lines,'move_lines')
         pickings = picking_record.move_lines
         for record in pickings:
-            print(record)
-            print(record.move_line_ids,'Enter into move_line_ids')
+            # print(record)
+            # print(record.move_line_ids,'Enter into move_line_ids')
             move_line_ids = record.move_line_ids
             for move in move_line_ids:
-                print(move)
+                # print(move)
                 vin = move.lot_id.name
-                print(vin,'vin_number')
+                # print(vin,'vin_number')
                 fleet_vehicle = self.env['fleet.vehicle'].sudo().search([('vin_sn','=',vin)])
-                print(fleet_vehicle,'fleet_vehicle')
-                print(fleet_vehicle.service_type_sequence,'service_type_sequence')
+                # print(fleet_vehicle,'fleet_vehicle')
+                # print(fleet_vehicle.service_type_sequence,'service_type_sequence')
                 return_services = self.env['service.type'].search([('sequence','=',fleet_vehicle.service_type_sequence)])
-                print(return_services, 'return_services')
+                # print(return_services, 'return_services')
                 if fleet_vehicle.service_type_sequence != 0:
                     raise ValidationError(f" Already you have done {return_services.name} ! "
                                                   f"You can not return the vehicle.")
